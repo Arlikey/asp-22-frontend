3a4248ca5618b60a48969245fe7a5a9ee1acfa49
admin <admin@DESKTOP-FA5KDQG>
2025-03-20 12:45:46 +0200

    created project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Home.js';
+import Category from './Category.js';
+import Layout from './Layout.js';
+import Product from './views/product/product.jsx';
+import { createContext } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const AppContext = createContext(null); 
+
+function App(){
+  const request = (url, conf) => new Promise((resolve, reject) =>  {
+    if(url.startsWith('/')){
+      url = "https://localhost:7117" + url;
+    }
+    fetch(url, conf)
+    .then(r => r.json())
+      .then(j => {
+        if(j.status.isSuccess){
+          resolve(j.data);
+        }
+        else {
+          reject(j);
+        }
+      })
+  });
+
+  return <AppContext.Provider value={{request}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="category/:id" element={<Category />} />
+            <Route path="product/:id" element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  </AppContext.Provider> 
+  
 }
 
 export default App;
